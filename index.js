81225272be2e88fd5c6aab6331b7219ed7a29ce7
Temesgen Tsegaye <temesgentsegaye9891@gmail.com>
2023-02-08 15:12:25 +0300

    fix script

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require("cors");
+const dotenv=require('dotenv')
 
+dotenv.config()
 const app = express();
 
 app.use(bodyparser.json());
